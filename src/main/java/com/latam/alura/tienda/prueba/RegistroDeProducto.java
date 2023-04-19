4b8760e98966a4078145885ed92000c9ebe8bf79
Sebastian <75150041+Gibbor-SAP@users.noreply.github.com>
2023-04-19 11:06:33 -0300

    first commit

---
+++ src/main/java/com/latam/alura/tienda/prueba/RegistroDeProducto.java
@@ -0,0 +1,32 @@
+package com.latam.alura.tienda.prueba;
+
+import java.math.BigDecimal;
+
+import javax.persistence.EntityManager;
+
+import com.latam.alura.tienda.dao.CategoriaDao;
+import com.latam.alura.tienda.dao.ProductoDao;
+import com.latam.alura.tienda.modelo.Categoria;
+import com.latam.alura.tienda.modelo.Producto;
+import com.latam.alura.tienda.utils.JPAUtils;
+
+public class RegistroDeProducto {
+
+	public static void main(String[] args) {
+		Categoria celulares = new Categoria("CELULARES");
+		Producto celular = new Producto("Samsung","Celular usado",new BigDecimal("1000"),celulares);
+		
+		EntityManager em = JPAUtils.getEntityManager(); //Iniciamos la conexion
+		
+		ProductoDao productoDao = new ProductoDao(em);
+		CategoriaDao categoriaDao = new CategoriaDao(em);
+		
+		em.getTransaction().begin(); // Iniciar la transacción
+		
+		categoriaDao.guardar(celulares);
+		productoDao.guardar(celular);
+
+		em.getTransaction().commit();
+		em.close();
+	}
+}
